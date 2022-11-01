09f10867ba2392679b0727a9fb1e0829ed77ef1b
Bamarcheti <bmarchetifiorin@gmail.com>
2022-11-01 16:49:51 -0400

    criação de novo bolão

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import cors from "@fastify/cors"
 import { PrismaClient } from "@prisma/client"
 import Fastify from "fastify"
+import ShortUniqueId from "short-unique-id"
+import { z } from "zod"
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -16,8 +18,30 @@ async function bootstrap() {
   })
 
   fastify.get('/pools/count', async () => {
+
     const count = await prisma.pool.count()
+
     return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+    
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    })
+
+    return reply.status(201).send({ code })
   })
 
   await fastify.listen({ port: 3333, /* host: '0.0.0.0'*/ })
