b44d390cf9b0d843d3aec1e7a8379094f87e32fa
Bamarcheti <bmarchetifiorin@gmail.com>
2022-11-01 11:43:50 -0400

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import cors from "@fastify/cors"
+import { PrismaClient } from "@prisma/client"
+import Fastify from "fastify"
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true,
+  })
+
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+    return { count }
+  })
+
+  await fastify.listen({ port: 3333, /* host: '0.0.0.0'*/ })
+}
+
+bootstrap()
