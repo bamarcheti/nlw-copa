a6c8fc281705bb78e404c0bca44bf98af5a298df
Bamarcheti <bmarchetifiorin@gmail.com>
2022-11-06 17:59:54 -0400

    redirecionando o usuário para rotas da aplicação

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,10 +1,16 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { AppRoutes } from './app.routes'
+import { NavigationContainer } from '@react-navigation/native';
+import { Box } from 'native-base';
+import { useAuth } from '../hooks/useAuth';
+import { SignIn } from '../screens/SignIn';
+import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const { user } = useAuth();
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="gray.900">
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SignIn />}
+      </NavigationContainer>
+    </Box>
   )
 }
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -5,24 +5,26 @@ import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
 
 export function SignIn() {
-	const { signIn, user } = useAuth();
+  const { signIn, isUserLoading } = useAuth();
 
-    return (
-        <Center flex={1} bgColor="gray.900" p={7}>
-            <Logo width={212} height={40} />
-            <Button
-								type="SECONDARY"
-                title="ENTRAR COM GOOGLE"
-                leftIcon={
-                    <Icon as={Fontisto} name="google" color="white" size="md" />
-                }
-								mt={12}
-								onPress={signIn}
-            />
-            <Text color="white" textAlign="center" mt={4}>
-                Não utilizamos nenhuma informação além {'\n'} 
-								do seu e-mail para criação de sua conta.
-            </Text>
-        </Center>
-    );
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        type="SECONDARY"
+        title="ENTRAR COM GOOGLE"
+        leftIcon={
+					<Icon as={Fontisto} name="google" color="white" size="md" />
+        }
+				mt={12}
+        onPress={signIn}
+				isLoading={isUserLoading}
+				_loading={{ _spinner: { color: 'white' } }}
+      />
+			<Text color="white" textAlign="center" mt={4}>
+				Não utilizamos nenhuma informação além {"\n"}
+				do seu e-mail para criação de sua conta.
+      </Text>
+    </Center>
+  );
 }
