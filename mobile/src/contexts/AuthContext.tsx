37ee31821618337253741a5b24167c0b344137c3
Bamarcheti <bmarchetifiorin@gmail.com>
2022-11-08 14:44:58 -0400

    adicionando variável de segurança

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -6,79 +6,82 @@ import { api } from "../services/api";
 
 WebBrowser.maybeCompleteAuthSession();
 
-export interface UserProps {
-    name: string;
-    avatarUrl: string;
+interface UserProps {
+  name: string;
+  avatarUrl: string;
 }
 
 export interface AuthContextDataProps {
-    user: UserProps;
-    isUserLoading: boolean;
-    signIn: () => Promise<void>;
+  user: UserProps;
+  isUserLoading: boolean;
+  signIn: () => Promise<void>;
 }
 
 interface AuthProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
-    const [user, setUser] = useState<UserProps>({} as UserProps);
-    const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [isUserLoading, setIsUserLoading] = useState(false);
 
-    const [request, response, promptAsync] = Google.useAuthRequest({
-        clientId:
-            "63898382960-1gtvs8ortv6smskbm0p6uqiu27us2utl.apps.googleusercontent.com",
-        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
-        scopes: ["profile", "email"],
-    });
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: process.env.CLIENT_ID,
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["profile", "email"],
+  });
 
-    async function signIn() {
-        try {
-            setIsUserLoading(true);
-            await promptAsync();
-        } catch (error) {
-            console.log(error);
-            throw error;
-        } finally {
-            setIsUserLoading(false);
-        }
+  async function signIn() {
+    try {
+        setIsUserLoading(true);
+        await promptAsync();
+    } catch (error) {
+        console.log(error);
+        throw error;
+    } finally {
+        setIsUserLoading(false);
     }
+  }
 
-    async function signInWithGoogle(access_token: string) {
-        try {
-          setIsUserLoading(true);
+  async function signInWithGoogle(access_token: string) {
+    try {
+      setIsUserLoading(true);
 
-          const tokenResponse = await api.post('/users', { access_token });
-          api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`;
-          
-          const userInfoResponse = await api.get('/me');
-          setUser(userInfoResponse.data.user);
-        } catch (error) {
-          console.log(error);
-          throw error;        
-        } finally {
-          setIsUserLoading(false);
-        }
-      }
-    
-    useEffect(() => {
-      if (response?.type === 'success' && response.authentication?.accessToken) {
-        signInWithGoogle(response.authentication.accessToken);
-      }
-    }, [response])
-    
+      const tokenResponse = await api.post("/users", { access_token });
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${tokenResponse.data.token}`;
 
-    return (
-        <AuthContext.Provider
-            value={{
-                signIn,
-                isUserLoading,
-                user,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+      const userInfoResponse = await api.get("/me");
+      setUser(userInfoResponse.data.user);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (
+      response?.type === "success" &&
+      response.authentication?.accessToken
+    ) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn,
+        isUserLoading,
+        user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
